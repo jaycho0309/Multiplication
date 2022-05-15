d487545be3edf36c8d364085cc383fd42b2c13bc
jaycho <jaycho0309@naver.com>
2022-05-15 13:58:49 +0900

    Add Test

--- src/main/java/microservices/book/socialmultiplication/multiplication/service/RandomGeneratorService.java
+++ src/main/java/microservices/book/socialmultiplication/multiplication/service/RandomGeneratorService.java
@@ -1,5 +1,7 @@
 package microservices.book.socialmultiplication.multiplication.service;
 
+import org.springframework.stereotype.Service;
+
 public interface RandomGeneratorService {
     int generateRandomFactor();
 }
